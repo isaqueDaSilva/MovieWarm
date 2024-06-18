7462ffa63e26e2997c9ae5f1cae3df4679f0a100
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-06-18 16:11:50 -0300

    Initial Commit

---
+++ MovieWarm/MovieWarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieWarmApp.swift
+//  MovieWarm
+//
+//  Created by Isaque da Silva on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieWarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
