93089408cdfe5c7aaf323a3d39b9e4ff39c295f1
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-06-19 02:27:59 -0300

    Initial Commit

---
+++ MovieWarm/Configuration.swift
@@ -0,0 +1,30 @@
+//
+//  ChoiceMovie+ViewMode.swift
+//  MovieWarm
+//
+//  Created by Isaque da Silva on 19/06/24.
+//
+
+import Foundation
+
+enum Configuration {
+    enum ConfigError: Error {
+        case missingKey, invalidValue
+    }
+
+    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
+            throw ConfigError.missingKey
+        }
+
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            throw ConfigError.invalidValue
+        }
+    }
+}
