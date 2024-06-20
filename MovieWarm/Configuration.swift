e749ea49beb524c48dce0cd197b4102b187ac37c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-06-20 20:43:48 -0300

    Criei o tipo Configuration Este tipo armazena um metódo que fará uma busca pelo bundle directory atrás de uma API_KEY, onde que este código será responsável por liberar o acesso para que possamos consumir o OMDb API.

--- MovieWarm/Configuration.swift
+++ MovieWarm/Configuration.swift
@@ -8,10 +8,6 @@
 import Foundation
 
 enum Configuration {
-    enum ConfigError: Error {
-        case missingKey, invalidValue
-    }
-
     static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
         guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
             throw ConfigError.missingKey
